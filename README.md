78d0397c31bfdee3aff5a5b3eebbaa86a664aae2
Maxwell Yang <gabrielyang233@outlook.com>
2022-01-08 17:27:47 -0500

    Add database models

---
+++ README.md
@@ -0,0 +1,4 @@
+# Frydgy
+Frydgy is a simple fridge management system. It allows users to log the items stored in the fridge into the system so that they can view the items in the fridge without having to open the fridge and see it.
+
+This is meant to be a practice project for me to learn Fastapi and some css frameworks, while at the same time refresh my memory in sqlalchemy
---
+++ models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer
+from sqlalchemy.orm import relationship
+
+from .database import Base
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True)
+    unit = Column(String)
+    quantity = Column(Integer, default=1)
+    compartmentId = Column(Integer, ForeignKey("compartments.id"))
+
+    compartment = relationship("Compartment", back_populates="items")
+
+class Compartment(Base):
+    __tablename__ = "compartments"
+
+    id = Column(Integer, primary_key=True, index=True)
+    Location = Column(String, unique=True)
+
+    items = relationship("Item", back_populates="compartment")
