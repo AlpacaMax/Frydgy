bb7974c5aee49cf13f588646faf28bb92cd88201
Maxwell Yang <gabrielyang233@outlook.com>
2022-01-10 18:33:53 -0500

    Add compartment in Item schema

--- crud.py
+++ crud.py
@@ -1,26 +1,29 @@
 from sqlalchemy.orm import Session
-from . import models, schemas
+from . import schemas
+from .models import Item, Compartment
 
 def seed(db: Session):
-    refrigerator = models.Compartment(name="refrigerator")
+    refrigerator = Compartment(name="refrigerator")
     db.add(refrigerator)
     db.commit()
     db.refresh(refrigerator)
 
-    freezer = models.Compartment(name="freezer")
+    freezer = Compartment(name="freezer")
     db.add(freezer)
     db.commit()
     db.refresh(refrigerator)
 
-    apples = models.Item(name="Apple", quantity=3, compartmentId=refrigerator.id)
+    apples = Item(name="Apple", quantity=3, compartmentId=refrigerator.id)
     db.add(apples)
     db.commit()
     db.refresh(apples)
 
-    banana = models.Item(name="Banana", quantity=2, compartmentId=freezer.id)
+    banana = Item(name="Banana", quantity=2, compartmentId=freezer.id)
     db.add(banana)
     db.commit()
     db.refresh(apples)
 
 def getAllItems(db: Session):
-    return db.query(models.Item).all()
+    return db.query(Item.name, Item.unit, Item.quantity, Compartment.name.label("compartment")).\
+              join(Compartment, Item.compartmentId==Compartment.id).\
+              all()
--- schemas.py
+++ schemas.py
@@ -1,10 +1,14 @@
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class Item(BaseModel):
     name: str
     unit: Optional[str]
-    quantity: int = 1
+    quantity: int = Field(
+        1,
+        ge=1,
+    )
+    compartment: str
 
     class Config:
         orm_mode = True
